9fab1fb7de873b747fb148b9c46fc31f3ed79501
rickyx12 <aizekrysler@gmail.com>
2016-01-28 20:23:18 +0800

    fix for startLetter during delete

--- COCONUT/inventory/stockCard/deleteStockCard.php
+++ COCONUT/inventory/stockCard/deleteStockCard.php
@@ -1,10 +1,11 @@
 <?php
 include("../../../myDatabase.php");
 $stockCardNo = $_GET['stockCardNo'];
+$startLetter = $_GET['startLetter'];
 $ro = new database();
 
 $ro->editNow("inventoryStockCard","stockCardNo",$stockCardNo,"status","DELETED");
 
-$ro->gotoPage("http://".$ro->getMyUrl()."/COCONUT/inventory/stockCard/stockCardList.php");
+$ro->gotoPage("http://".$ro->getMyUrl()."/COCONUT/inventory/stockCard/stockCardList.php?startLetter=$startLetter");
 
 ?>
