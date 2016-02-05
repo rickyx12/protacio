a6c32a965a6443febfc3950a2b75b17f4d3b5fba
rickyx12 <aizekrysler@gmail.com>
2016-02-05 20:18:02 +0800

    add 1 row in total as space and make font larger in total

--- COCONUT/Cashier/cashierReport/dailyCashiersReport.php
+++ COCONUT/Cashier/cashierReport/dailyCashiersReport.php
@@ -40,12 +40,22 @@ echo "<th>Type</th>";
 echo "</tr>";
 $ro->dailyCashiersReport_ipd($shift,$date,$username);
 $ro->dailyCashiersReport_opd($shift,$date,$username);
+
+echo "<Tr>";
+echo "<td>&nbsp;</td>";
+echo "<td>&nbsp;</td>";
+echo "<td>&nbsp;</td>";
+echo "<td>&nbsp;</td>";
+echo "<td>&nbsp;</td>";
+echo "<td>&nbsp;</td>";
+echo "</tr>";
+
 echo "<tr>";
 echo "<td>&nbsp;</td>";
 echo "<td>&nbsp;</td>";
 echo "<td>&nbsp;</td>";
-echo "<td>&nbsp;<font size=2>TOTAL</font></td>";
-echo "<td>&nbsp;<font size=2>".number_format($ro->dailyCashiersReport_opd_total() + $ro->dailyCashiersReport_ipd_total(),2)."</font></td>";
+echo "<td>&nbsp;<font size=3>TOTAL</font></td>";
+echo "<td>&nbsp;<font size=3>".number_format($ro->dailyCashiersReport_opd_total() + $ro->dailyCashiersReport_ipd_total(),2)."</font></td>";
 echo "<td>&nbsp;</td>";
 echo "</tr>";
 echo "</table>";
