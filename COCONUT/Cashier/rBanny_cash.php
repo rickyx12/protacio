81b34652f6f963106c0ba0d133f29a3d837a14d7
rickyx12 <aizekrysler@gmail.com>
2016-02-10 14:41:01 +0800

    remove inserting of balance in registrationDetails tbl

--- COCONUT/Cashier/rBanny_cash.php
+++ COCONUT/Cashier/rBanny_cash.php
@@ -48,7 +48,7 @@ $ro->editNow("registrationDetails","registrationNo",$registrationNo,"dateUnregis
 $ro->editNow("registrationDetails","registrationNo",$registrationNo,"timeUnregistered",date("H:i:s")); //time discharged
 $ro->editNow("registrationDetails","registrationNo",$registrationNo,"mgh","Synapse System"); //set as MGH [LOCKED ACCOUNT] 
 $ro->editNow("registrationDetails","registrationNo",$registrationNo,"mgh_date",date("Y-m-d")); //set as MGH [LOCKED ACCOUNT] 
-$ro->editNow("registrationDetails","registrationNo",$registrationNo,"balance",$ro->getTotal("cashUnpaid","",$registrationNo)); 
+//$ro->editNow("registrationDetails","registrationNo",$registrationNo,"balance",$ro->getTotal("cashUnpaid","",$registrationNo)); 
 $ro->addDischargeHistory($registrationNo,"Closed",date("H:i:s"),date("Y-m-d"),$username);
 }else {
 echo "2";
@@ -79,7 +79,7 @@ $ro->editNow("registrationDetails","registrationNo",$registrationNo,"dateUnregis
 $ro->editNow("registrationDetails","registrationNo",$registrationNo,"timeUnregistered",date("H:i:s")); //time discharged
 $ro->editNow("registrationDetails","registrationNo",$registrationNo,"mgh","Synapse System"); //set as MGH [LOCKED ACCOUNT] 
 $ro->editNow("registrationDetails","registrationNo",$registrationNo,"mgh_date",date("Y-m-d")); //set as MGH [LOCKED ACCOUNT] 
-$ro->editNow("registrationDetails","registrationNo",$registrationNo,"balance",$ro->getTotal("cashUnpaid","",$registrationNo)); 
+//$ro->editNow("registrationDetails","registrationNo",$registrationNo,"balance",$ro->getTotal("cashUnpaid","",$registrationNo)); 
 }else {
 $excessPaid  = ( $targetAmount - $itemz[0] );
 $ro->editNow("patientCharges","itemNo",$itemz[1],"cashUnpaid","0.00");
@@ -111,7 +111,7 @@ $excessPaid2 = $excessPaid1;
 if( $excessPaid2 > 0 ) {
 $ro->gotoPage("http://".$ro->getMyUrl()."/COCONUT/Cashier/rBanny_cash.php?registrationNo=$registrationNo&cashInputted=$cashInputted&targetAmount=$excessPaid1&username=$username&orNO=$orNO&datePaid=$datePaid&shift=$shift");
 }else {
-$ro->editNow("registrationDetails","registrationNo",$registrationNo,"balance",$ro->getTotal("cashUnpaid","",$registrationNo));
+//$ro->editNow("registrationDetails","registrationNo",$registrationNo,"balance",$ro->getTotal("cashUnpaid","",$registrationNo));
 $ro->gotoPage("http://".$ro->getMyUrl()."/COCONUT/patientProfile/individualPayment/toDispense.php?registrationNo=$registrationNo&module=PHARMACY&username=$username&month=".date("m")."&day=".date("d")."&year=".date("Y")."&fromTime_hour=00&fromTime_minutes=00&fromTime_seconds=00&toTime_hour=24&toTime_minutes=00&toTime_seconds=00&nod=");
 }
 
