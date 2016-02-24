ae04fc1bc5491709dd43efbf754b3ebaeac35155
rickyx12 <aizekrysler@gmail.com>
2016-02-24 18:33:01 +0800

    get exact amount of charges when payment assigning

--- COCONUT/patientProfile/Payments/assignPayment.php
+++ COCONUT/patientProfile/Payments/assignPayment.php
@@ -14,24 +14,24 @@ $assignPayment = preg_split ("/\_/", $assign[$x]);
 $ro->getPatientChargesToEdit($assignPayment[0]);
 
 if($assignPayment[1] == "cash") {
-$ro->EditNow("patientCharges","itemNo",$assignPayment[0],"cashUnpaid",($ro->patientCharges_total() + $ro->patientCharges_discount())); // iLLgay sa cash
+$ro->EditNow("patientCharges","itemNo",$assignPayment[0],"cashUnpaid",($ro->selectNow("patientCharges","sellingPrice","itemNo",$assignPayment[0]) * $ro->selectNow("patientCharges","quantity","itemNo",$assignPayment[0]))); // iLLgay sa cash
 ///ccguraduhin n mgging zero ung total sa mga column mga column n e2 dahil mppunta Lhat sa cash
 $ro->EditNow("patientCharges","itemNo",$assignPayment[0],"company",0);
 $ro->EditNow("patientCharges","itemNo",$assignPayment[0],"phic",0);
 $ro->EditNow("patientCharges","itemNo",$assignPayment[0],"discount",0);
-$ro->EditNow("patientCharges","itemNo",$assignPayment[0],"total",($ro->patientCharges_total() + $ro->patientCharges_discount()));
+$ro->EditNow("patientCharges","itemNo",$assignPayment[0],"total",($ro->selectNow("patientCharges","sellingPrice","itemNo",$assignPayment[0]) * $ro->selectNow("patientCharges","quantity","itemNo",$assignPayment[0])));
 }else if($assignPayment[1] == "hmo") {
-$ro->EditNow("patientCharges","itemNo",$assignPayment[0],"company",($ro->patientCharges_total() + $ro->patientCharges_discount()));
+$ro->EditNow("patientCharges","itemNo",$assignPayment[0],"company",($ro->selectNow("patientCharges","sellingPrice","itemNo",$assignPayment[0]) * $ro->selectNow("patientCharges","quantity","itemNo",$assignPayment[0])));
 $ro->EditNow("patientCharges","itemNo",$assignPayment[0],"cashUnpaid",0);
 $ro->EditNow("patientCharges","itemNo",$assignPayment[0],"phic",0);
 $ro->EditNow("patientCharges","itemNo",$assignPayment[0],"discount",0);
-$ro->EditNow("patientCharges","itemNo",$assignPayment[0],"total",($ro->patientCharges_total() + $ro->patientCharges_discount()));
+$ro->EditNow("patientCharges","itemNo",$assignPayment[0],"total",($ro->selectNow("patientCharges","sellingPrice","itemNo",$assignPayment[0]) * $ro->selectNow("patientCharges","quantity","itemNo",$assignPayment[0])));
 }else if($assignPayment[1] == "phic") {
-$ro->EditNow("patientCharges","itemNo",$assignPayment[0],"phic",($ro->patientCharges_total() + $ro->patientCharges_discount()));
+$ro->EditNow("patientCharges","itemNo",$assignPayment[0],"phic",($ro->selectNow("patientCharges","sellingPrice","itemNo",$assignPayment[0]) * $ro->selectNow("patientCharges","quantity","itemNo",$assignPayment[0])));
 $ro->EditNow("patientCharges","itemNo",$assignPayment[0],"cashUnpaid",0);
 $ro->EditNow("patientCharges","itemNo",$assignPayment[0],"company",0);
 $ro->EditNow("patientCharges","itemNo",$assignPayment[0],"discount",0);
-$ro->EditNow("patientCharges","itemNo",$assignPayment[0],"total",($ro->patientCharges_total() + $ro->patientCharges_discount()));
+$ro->EditNow("patientCharges","itemNo",$assignPayment[0],"total",($ro->selectNow("patientCharges","sellingPrice","itemNo",$assignPayment[0]) * $ro->selectNow("patientCharges","quantity","itemNo",$assignPayment[0])));
 }else {
 echo "";
 }
