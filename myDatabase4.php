e58dc477bee9fc6e8aa04a8bc311582bfa9b0303
rickyx12 <aizekrysler@gmail.com>
2016-03-03 17:25:49 +0800

    report for Daily Transaction

--- myDatabase4.php
+++ myDatabase4.php
@@ -168,6 +168,166 @@ public function companyList() {
 }
 }
 
+private $patient_with_transaction_registrationNo;
+private $patient_with_transaction_lastName;
+private $patient_with_transaction_firstName;
+private $patient_with_transaction_patientCompany;
+private $patient_with_transaction_pxCount;
+
+public function patient_with_transaction_registrationNo() {
+	return $this->patient_with_transaction_registrationNo;
+}
+public function patient_with_transaction_lastName() {
+	return $this->patient_with_transaction_lastName;
+}
+public function patient_with_transaction_firstName() {
+	return $this->patient_with_transaction_firstName;
+}
+public function patient_with_transaction_patientCompany() {
+	return $this->patient_with_transaction_patientCompany;
+}
+public function patient_with_transaction_pxCount() {
+	return $this->patient_with_transaction_pxCount;
+}
+
+public function patient_with_transaction($date) {
+	$connection = mysqli_connect($this->host,$this->username,$this->password,$this->database);      
+	$result = mysqli_query($connection, "select upper(pr.lastName) as lastName,upper(pr.firstName) as firstName,rd.registrationNo,rd.Company,rd.pxCount from patientRecord pr,registrationDetails rd where pr.patientNo = rd.patientNo and rd.dateUnregistered = '$date' and rd.type = 'OPD' order by pxCount asc ") or die("Query fail: " . mysqli_error()); 
+	while($row = mysqli_fetch_array($result)) {
+	$this->patient_with_transaction_registrationNo[] = $row['registrationNo'];
+	$this->patient_with_transaction_lastName[] = $row['lastName'];
+	$this->patient_with_transaction_firstName[] = $row['firstName'];
+	$this->patient_with_transaction_patientCompany[] = $row['Company'];
+	$this->patient_with_transaction_pxCount[] = $row['pxCount'];
+	}
+}
+
+
+public function patient_with_transaction_total($registrationNo) {
+	$connection = mysqli_connect($this->host,$this->username,$this->password,$this->database);      
+	$result = mysqli_query($connection, "select sum(total) as total from patientCharges where registrationNo = '$registrationNo' and status not like 'DELETED%%%' ") or die("Query fail: " . mysqli_error()); 
+
+	while($row = mysqli_fetch_array($result)) {
+		($row['total'] > 0) ? $x = $row['total'] : $x = 0;
+		return $x;
+	}
+}
+
+public function patient_with_transaction_balance($registrationNo) {
+	$connection = mysqli_connect($this->host,$this->username,$this->password,$this->database);      
+	$result = mysqli_query($connection, "select sum(cashUnpaid) as cashUnpaid from patientCharges where registrationNo = '$registrationNo' and status not like 'DELETED%%%' ") or die("Query fail: " . mysqli_error()); 
+
+	while($row = mysqli_fetch_array($result)) {
+		($row['cashUnpaid'] > 0) ? $x = $row['cashUnpaid'] : $x = 0;
+		return $x;
+	}
+}
+
+public function patient_with_transaction_company($registrationNo) {
+	$connection = mysqli_connect($this->host,$this->username,$this->password,$this->database);      
+	$result = mysqli_query($connection, "select sum(company) as company from patientCharges where registrationNo = '$registrationNo' and status not like 'DELETED%%%' ") or die("Query fail: " . mysqli_error()); 
+
+	while($row = mysqli_fetch_array($result)) {
+		($row['company'] > 0) ? $x = $row['company'] : $x = 0;
+		return $x;
+	}
+}
+
+
+public function patient_with_transaction_cash($registrationNo) {
+	$connection = mysqli_connect($this->host,$this->username,$this->password,$this->database);      
+	$result = mysqli_query($connection, "select sum(cashPaid) as pd from patientCharges where registrationNo = '$registrationNo' and status not like 'DELETED%%%' and paidVia = 'Cash' ") or die("Query fail: " . mysqli_error()); 
+
+	while($row = mysqli_fetch_array($result)) {
+		($row['pd'] > 0) ? $x = $row['pd'] : $x = 0;
+		return $x;
+	}
+}
+
+public function patient_with_transaction_creditCard($registrationNo) {
+	$connection = mysqli_connect($this->host,$this->username,$this->password,$this->database);      
+	$result = mysqli_query($connection, "select sum(amountPaidFromCreditCard) as pd from patientCharges where registrationNo = '$registrationNo' and status not like 'DELETED%%%' and paidVia = 'Credit Card' ") or die("Query fail: " . mysqli_error()); 
+
+	while($row = mysqli_fetch_array($result)) {
+		($row['pd'] > 0) ? $x = $row['pd'] : $x = 0;
+		return $x;
+	}
+}
+
+
+private $inpatient_payment_lastName;
+private $inpatient_payment_firstName;
+private $inpatient_payment_paymentFor;
+private $inpatient_payment_registrationNo;
+
+public function inpatient_payment_lastName() {
+	return $this->inpatient_payment_lastName;
+}
+public function inpatient_payment_firstName() {
+	return $this->inpatient_payment_firstName;
+}
+public function inpatient_payment_paymentFor() {
+	return $this->inpatient_payment_paymentFor;
+}
+public function inpatient_payment_registrationNo() {
+	return $this->inpatient_payment_registrationNo;
+}
+
+public function inpatient_payment($date) {
+	$connection = mysqli_connect($this->host,$this->username,$this->password,$this->database);      
+	$result = mysqli_query($connection, "select upper(pr.lastName) as lastName,upper(pr.firstName) as firstName,rd.registrationNo,pp.paymentFor,pp.amountPaid from patientRecord pr,registrationDetails rd,patientPayment pp where pr.patientNo = rd.patientNo and rd.registrationNo = pp.registrationNo and pp.datePaid = '$date' and rd.type = 'IPD' ") or die("Query fail: " . mysqli_error()); 
+
+	while($row = mysqli_fetch_array($result)) {
+		$this->inpatient_payment_lastName[] = $row['lastName'];
+		$this->inpatient_payment_firstName[] = $row['firstName'];
+		$this->inpatient_payment_paymentFor[] = $row['paymentFor'];
+		$this->inpatient_payment_registrationNo[] = $row['registrationNo'];
+			}
+}
+
+
+public function inpatient_payment_paid($registrationNo,$paymentMode) {
+	$connection = mysqli_connect($this->host,$this->username,$this->password,$this->database);      
+	$result = mysqli_query($connection, "select amountPaid as pd from patientPayment where registrationNo = '$registrationNo' and paidVia = '$paymentMode'  ") or die("Query fail: " . mysqli_error()); 
+
+	while($row = mysqli_fetch_array($result)) {
+		($row['pd'] > 0) ? $x = $row['pd'] : $x = 0;
+		return $x;
+	}
+}
+
+
+private $inventory_list_inventoryCode;
+private $inventory_list_stockCardNo;
+private $inventory_list_description;
+private $inventory_list_genericName;
+
+public function inventory_list_inventoryCode() {
+	return $this->inventory_list_inventoryCode;
+}
+public function inventory_list_stockCardNo() {
+	return $this->inventory_list_stockCardNo;
+}
+public function inventory_list_description() {
+	return $this->inventory_list_description;
+}
+public function inventory_list_genericName() {
+	return $this->inventory_list_genericName;
+}
+
+public function inventory_list($inventoryType) {
+	$connection = mysqli_connect($this->host,$this->username,$this->password,$this->database);      
+	$result = mysqli_query($connection, "select i.inventoryCode,i.stockCardNo,i.description,i.genericName,i.quantity from inventory i order by i.genericName and i.status not like 'DELETED' order by i.genericName asc ") or die("Query fail: " . mysqli_error()); 
+
+	while($row = mysqli_fetch_array($result)) {
+		$this->inventory_list_inventoryCode[] = $row['inventoryCode'];
+		$this->inventory_list_stockCardNo[] = $row['stockCardNo'];
+		$this->inventory_list_description[] = $row['description'];
+		$this->inventory_list_genericName[] = $row['genericName'];
+	}
+}
+
+
 
 
 }
