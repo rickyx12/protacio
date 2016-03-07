4af012868771e44bf86e0eaee6dc6e224a5c167d
rickyx12 <aizekrysler@gmail.com>
2016-03-07 12:46:50 +0800

    add letter U in start letter

--- COCONUT/inventory/stockCard/sortStockCard.php
+++ COCONUT/inventory/stockCard/sortStockCard.php
@@ -9,7 +9,7 @@ echo "<center><font size=2 color=blue>Generic Name Start with letter</font></cen
 $ro->coconutBoxStart("600","70");
 echo "<Br>";
 
-$startLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','V','W','X','Y','Z'];
+$startLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 for($x=0;$x<=24;$x++) {
 echo "<a href='stockCardList.php?startLetter=".$startLetter[$x]."' style='text-decoration:none; color:red;'>".$startLetter[$x]."</a>&nbsp;&nbsp;";
