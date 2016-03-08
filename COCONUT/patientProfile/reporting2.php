b4a962d2d5b2b6429d68d04d686e76f4e735473f
rickyx12 <aizekrysler@gmail.com>
2016-03-08 16:54:41 +0800

    for dailyCashier edit reportShift and paidDate

--- COCONUT/patientProfile/reporting2.php
+++ COCONUT/patientProfile/reporting2.php
@@ -11,7 +11,8 @@ $ro = new database3();
 for( $x=0;$x<$count;$x++ ) {
 //$ro->editHistory($itemNo[$x],$reportDate,$reportShift);
 echo "<font color=red>".$ro->selectNow("patientCharges","description","itemNo",$itemNo[$x])."</font> Reporting Date:&nbsp;$reportDate......Reporting Shift:&nbsp;$reportShift<Br>";
-
+$ro->editNow("patientCharges","itemNo",$itemNo[$x],"reportShift",$reportShift);
+$ro->editNow("patientCharges","itemNo",$itemNo[$x],"datePaid",$reportDate);
 }
 
 
