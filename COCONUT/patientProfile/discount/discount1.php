74b696df13f9f55744d497542078f597f9cbe6ae
rickyx12 <aizekrysler@gmail.com>
2016-03-13 18:43:08 +0800

    fix for discount when px is company

--- COCONUT/patientProfile/discount/discount1.php
+++ COCONUT/patientProfile/discount/discount1.php
@@ -16,7 +16,15 @@ $disc1 = round($disc);
 //$ro->doubleEditNow("patientCharges","itemNo",$itemNo[$x],"registrationNo",$registrationNo,"sellingPrice",$sellingPrice);
 //$ro->doubleEditNow("patientCharges","itemNo",$itemNo[$x],"registrationNo",$registrationNo,"total",($sellingPrice * $ro->selectNow("patientCharges","quantity","itemNo",$itemNo[$x]) ) - $disc1 );
 $ro->doubleEditNow("patientCharges","itemNo",$itemNo[$x],"registrationNo",$registrationNo,"discount",$disc1);
+
+if($ro->selectNow("patientCharges","cashUnpaid","itemNo",$itemNo[$x]) > 0 ) {
 $ro->doubleEditNow("patientCharges","itemNo",$itemNo[$x],"registrationNo",$registrationNo,"cashUnpaid",($sellingPrice * $ro->selectNow("patientCharges","quantity","itemNo",$itemNo[$x]) -$disc1 ));
+}else if($ro->selectNow("patientCharges","company","itemNo",$itemNo[$x])) {
+$ro->doubleEditNow("patientCharges","itemNo",$itemNo[$x],"registrationNo",$registrationNo,"company",($sellingPrice * $ro->selectNow("patientCharges","quantity","itemNo",$itemNo[$x]) -$disc1 ));
+}else {
+	echo "Ooops ERROR.";
+}
+
 }
 
 echo "<font color=red>Discount Applied.!</font>";
