2173c41767efe17fe2c19fbe1a0ec8c6c28ab636
rickyx12 <aizekrysler@gmail.com>
2016-04-08 18:59:06 +0800

    add the Update button for the editing function in new interface of inventory

--- COCONUT/inventory/inventory-new.php
+++ COCONUT/inventory/inventory-new.php
@@ -25,6 +25,31 @@
 					});
 				});
 
+
+				$(document).on("click","#updateNow<?php echo $inventoryCode ?>",function(){
+
+					var description = $("#description<?php echo $inventoryCode ?>").val();
+					var generic = $("#generic<?php echo $inventoryCode ?>").val();
+					var quantity = $("#quantity<?php echo $inventoryCode ?>").val();
+					var unitcost = $("#unitcost<?php echo $inventoryCode ?>").val();
+					var ipdPrice = $("#ipdPrice<?php echo $inventoryCode ?>").val();
+					var opdPrice = $("#opdPrice<?php echo $inventoryCode ?>").val();
+
+					var myData = {
+						inventoryCode:<?php echo $inventoryCode ?>,
+						description:description,
+						generic:generic,
+						quantity:quantity,
+						unitcost:unitcost,
+						ipdPrice:ipdPrice,
+						opdPrice:opdPrice
+					}
+
+					$.post("inventory-new-update.php",myData,function(data) {
+						$("#myTable").load("inventory-new.php #myTable");
+					})
+				})
+
 			<?php } ?>
 
 		});
@@ -50,6 +75,7 @@
 					<th>Inpatient</th>
 					<th>Outpatient</th>
 					<th></th>
+					<th></th>
 				</tr>
 			</thead>
 			<tbody>
@@ -63,7 +89,8 @@
 					<td>&nbsp;<?php echo $ro->number_format($ro1->selectNow("inventory","unitcost","inventoryCode",$inventoryCode)) ?></td>
 					<td>&nbsp;<?php echo $ro->number_format($ro1->selectNow("inventory","ipdPrice","inventoryCode",$inventoryCode)) ?></td>
 					<td>&nbsp;<?php echo $ro->number_format($ro1->selectNow("inventory","opdPrice","inventoryCode",$inventoryCode)) ?></td>
-					<td><button type="button" class="btn btn-danger" data-toggle="modal" data-target="#removeModal<?php echo $inventoryCode ?>">Remove</button></td>
+					<td><button type="button" class="btn btn-success" data-toggle="modal" data-target="#updateModal<?php echo $inventoryCode ?>">Update</button></td>
+					<td><button type="button" class="btn btn-danger" data-toggle="modal" data-target="#removeModal<?php echo $inventoryCode ?>">Remove</button></td>					
 				</tr>
 			<?php } ?>
 			</tbody>
@@ -87,6 +114,54 @@
 				</div>
 			</div>
 		</div>
+
+
+
+		<div id="updateModal<?php echo $inventoryCode ?>" class="modal fade" role="dialog">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h4 class="modal-title"><?php echo $ro1->selectNow('inventory','description','inventoryCode',$inventoryCode) ?></h4>
+					</div>
+					<div class="modal-body">
+						<div class="container">
+						<div class="col-md-4">
+							<div class="form-group">
+								<label>Brand</label>
+								<input type="text" id="description<?php echo $inventoryCode ?>" class="form-control" value="<?php echo $ro1->selectNow('inventory','description','inventoryCode',$inventoryCode) ?>">
+							</div>
+							<div class="form-group">
+								<label>Generic</label>
+								<input type="text" id="generic<?php echo $inventoryCode ?>" class="form-control" value="<?php echo $ro1->selectNow('inventory','genericName','inventoryCode',$inventoryCode) ?>">
+							</div>
+							<div class="form-group">
+								<label>Quantity</label>
+								<input type="text" id="quantity<?php echo $inventoryCode ?>" class="form-control" value="<?php echo $ro1->selectNow('inventory','quantity','inventoryCode',$inventoryCode) ?>">
+							</div>
+							<div class="form-group">
+								<label>Unitcost</label>
+								<input type="text" id="unitcost<?php echo $inventoryCode ?>" class="form-control" value="<?php echo $ro1->selectNow('inventory','unitcost','inventoryCode',$inventoryCode) ?>">
+							</div>
+							<div class="form-group">
+								<label>Inpatient Price</label>
+								<input type="text" id="ipdPrice<?php echo $inventoryCode ?>" class="form-control" value="<?php echo $ro1->selectNow('inventory','ipdPrice','inventoryCode',$inventoryCode) ?>">
+							</div>
+							<div class="form-group">
+								<label>Outpatient Price</label>
+								<input type="text" id="opdPrice<?php echo $inventoryCode ?>" class="form-control" value="<?php echo $ro1->selectNow('inventory','opdPrice','inventoryCode',$inventoryCode) ?>">
+							</div>
+						</div>
+						</div>
+					</div>
+					<div class="modal-footer">
+						 <button type="button" class="btn btn-success" data-dismiss="modal">Cancel</button>
+						  <button id="updateNow<?php echo $inventoryCode ?>" type="button" class="btn btn-danger" data-dismiss="modal">Update</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
 		<?php } ?>
 
 	</div>
