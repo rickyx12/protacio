f245348ea377361c0249c44dc7f82ee4f92fd7e7
rickyx12 <aizekrysler@gmail.com>
2016-04-11 16:59:47 +0800

    fix bootstrap path

--- COCONUT/flow/patientTransaction.php
+++ COCONUT/flow/patientTransaction.php
@@ -184,7 +184,7 @@ $ipd_creditCard = 0;
   <title>Daily Transaction</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://localhost/bootstrap-3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../..//bootstrap-3.3.6/css/bootstrap.min.css">
   <script src="/bootstrap-3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
