54a2618ad2d0de9a467e476e5bc1a057a1d4b3cb
rickyx12 <aizekrysler@gmail.com>
2016-04-18 13:29:40 +0800

    manual inputted discount

---
+++ COCONUT/patientProfile/manual-discount.php
@@ -0,0 +1,103 @@
+<? include "../../myDatabase.php" ?>
+<? include "../../myDatabase4.php" ?>
+<? $ro = new database() ?>
+<? $ro4 = new database4() ?>
+<!doctype html>
+<html>
+	<head>
+		<title></title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script src="../../jquery-2.1.4.min.js"></script>
+		<link rel="stylesheet" href="../../bootstrap-3.3.6/css/bootstrap.css"></link>
+		<script src="../../bootstrap-3.3.6/js/bootstrap.js"></script>
+
+		<script>
+			
+			$(document).ready(function(){
+					
+				$("#alert").hide();
+
+				$("#discountBtn").click(function(){
+				var checkedVal = $('input[class=itemNo]:checked').map(function(){
+									return this.value;
+								}).get();
+				var discount = $("#discount").val();
+									
+					$.post("manual-discount1.php",{itemNo:checkedVal,discount:discount},function(data) {
+						$("#alert").show();
+						$("#alert").html(data);
+						$("#charges").load("manual-discount.php?registrationNo=<? echo $_GET['registrationNo'] ?> #charges");
+						$("#discount").val("");
+					});
+				});
+
+
+			});
+		
+		</script>
+
+	</head>
+	<body>
+		<? $ro4->get_patient_charges($_GET['registrationNo']) ?>
+		<!--
+		<form method="post" action="manual-discount1.php">
+		-->
+		<div class="container">
+			<div class="row">
+				<h3>Manual Discount</h3>
+				<div class="col-md-3">
+					<input type="text" id="discount" name="discount" class="form-control" placeholder="Enter discount" autocomplete="off">
+				</div>
+			</div>
+			<div class="row">
+				&nbsp;
+			</div>
+			<div class="row">
+				<div class="col-md-7">
+				<div id="alert" class="alert alert-info"></div>
+				<table id="charges" class="table table-hover">
+					<thead>
+						<tr>
+							<th>Description</th>
+							<th>Price</th>
+							<th>QTY</th>
+							<th>Diiscount</th>
+							<th>Total</th>
+							<th>Unpaid</th>
+							<th>&nbsp;</th>
+						</tr>
+					</thead>
+					<tbody>
+						<? foreach($ro4->get_patient_charges_itemNo() as $itemNo) { ?>
+							<tr>
+								<td><? echo $ro->selectNow("patientCharges","description","itemNo",$itemNo) ?></td>
+								<td><? echo $ro->selectNow("patientCharges","sellingPrice","itemNo",$itemNo) ?></td>
+								<td><? echo $ro->selectNow("patientCharges","quantity","itemNo",$itemNo) ?></td>
+								<td><? echo $ro->selectNow("patientCharges","discount","itemNo",$itemNo) ?></td>
+								<td><? echo $ro->selectNow("patientCharges","total","itemNo",$itemNo) ?></td>
+								<td><? echo $ro->selectNow("patientCharges","cashUnpaid","itemNo",$itemNo) ?></td>
+
+								<? if($ro->selectNow("patientCharges","discount","itemNo",$itemNo) < 1) { ?>
+									<td><input type="checkbox" id="itemNo<? echo $itemNo ?>" class="itemNo" name="itemNo[]" value="<? echo $itemNo ?>"></td>
+								<? }else { ?>
+									<td>&nbsp;</td>
+								<? } ?>
+							</tr>
+						<? } ?>
+					</tbody>
+
+				</table>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-7 text-right">
+					<input type="submit" id="discountBtn" class="btn btn-success" value="Add Discount">
+				</div>
+			</div>
+		</div>
+		<!--
+		</form>
+		-->
+	</body>
+</html>
---
+++ COCONUT/patientProfile/manual-discount1.php
@@ -0,0 +1,34 @@
+<?php
+include "../../myDatabase.php";
+
+$itemNo = $_POST['itemNo'];
+$discount = $_POST['discount'];
+$countCharges = 0;
+
+$ro = new database();
+
+echo "Entered Discount:&nbsp;".$discount."<br>";
+for($x=0;$x<count($itemNo);$x++){
+	//echo $itemNo[$x]."<br>";
+	$countCharges += 1;
+}
+$discountPerCharges = ($discount / $countCharges);
+//echo $countCharges;
+echo "Discount per Charges:&nbsp;".$discountPerCharges;
+
+//check ung charges kung mas mataas ung cashUnpaid kaysa sa discount n pde ilagay
+for($a=0;$a<count($itemNo);$a++) {
+	if($ro->selectNow("patientCharges","cashUnpaid","itemNo",$itemNo[$a]) < ($discount / $countCharges)) {
+		//dont execute some charges are not applicable for discount
+		echo "<br>[<font color=red>".$ro->selectNow("patientCharges","description","itemNo",$itemNo[$a])." - ".$ro->selectNow("patientCharges","cashUnpaid","itemNo",$itemNo[$a])."]</font> - not applicable for discount"."<br>";
+	}else {
+		//execute all charges are applicable for discount
+		$chargesUnpaid = $ro->selectNow("patientCharges","cashUnpaid","itemNo",$itemNo[$a]);
+		$ro->editNow("patientCharges","itemNo",$itemNo[$a],"discount",$discountPerCharges);
+		$ro->editNow("patientCharges","itemNo",$itemNo[$a],"cashUnpaid",($chargesUnpaid-$discountPerCharges));
+		echo "<Br>Discount Successful<br>";
+	}	
+}	
+
+
+?>
