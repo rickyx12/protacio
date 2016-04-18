4ace504ba3efb2ca6b689201aaf3242cc4c89284
rickyx12 <aizekrysler@gmail.com>
2016-04-18 17:07:26 +0800

    add button for saved ending inventory in omg

--- COCONUT/flow/menuz.php
+++ COCONUT/flow/menuz.php
@@ -136,4 +136,9 @@ echo "<input type='hidden' name='inventoryType' value='medicine'>";
 echo "<input type='submit' value='Ending Inv' style='border:1px solid #ff0000; width:100%; height:10%; color:blue; '>";
 echo "</form>";
 
+echo "<form method='get' action='http://".$ro->getMyUrl()."/COCONUT/inventory/ending-inventory-medicine.php' target='rightFrame'>";
+echo "<input type='hidden' name='inventoryType' value='medicine'>";
+echo "<input type='submit' value='Ending Inv List' style='border:1px solid #ff0000; width:100%; height:10%; color:blue; '>";
+echo "</form>";
+
 ?>
