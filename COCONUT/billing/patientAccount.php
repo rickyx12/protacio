71bd955b6716cd0f025a8be43047a80cf56795cc
rickyx12 <aizekrysler@gmail.com>
2016-04-20 14:20:10 +0800

    show Hospital and PF share when title is OT/PT

--- COCONUT/billing/patientAccount.php
+++ COCONUT/billing/patientAccount.php
@@ -16,30 +16,37 @@ $ro->patientAccount($date,$date1,$type,$title);
 }else {
 echo "<Br><br><center>";
 echo "<Table border=0 width='90%'>";
-echo "<tr>";
-echo "<th>Patient</th>";
-echo "<th>Particulars</th>";
-echo "<th>Disc</th>";
-echo "<th>UNPAID</th>";
-echo "<th>HMO</th>";
-echo "<th>PHIC</th>";
-echo "<th>CASH</th>";
-echo "<th>Cr.Card</th>";
-echo "<th>Total</th>";
-echo "</tr>";	
-$ro->patientAccountOPD_notPaid($date,$date1,$title);
-$ro->patientAccountOPD_paid($date,$date1,$title);
-echo "<tr>";
-echo "<td>&nbsp;</td>";
-echo "<td>&nbsp;</td>";
-echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_discount)."</td>";
-echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_unpaid)."</td>";
-echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_hmo)."</td>";
-echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_phic)."</td>";
-echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_cashpaid)."</td>";
-echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_creditCard)."</td>";
-echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_total)."</td>";
-echo "</tr>";
+	echo "<tr>";
+		echo "<th>Patient</th>";
+		echo "<th>Particulars</th>";
+		echo "<th>Disc</th>";
+		echo "<th>UNPAID</th>";
+		echo "<th>HMO</th>";
+		echo "<th>PHIC</th>";
+		echo "<th>CASH</th>";
+		echo "<th>Cr.Card</th>";
+		echo "<th>Total</th>";
+		if($title == "OT" || $title == "PT") {
+			echo "<th>Hospital</th>";
+			echo "<th>PF</th>";
+		}else {	
+			/*hide*/
+		}
+
+	echo "</tr>";	
+	$ro->patientAccountOPD_notPaid($date,$date1,$title);
+	$ro->patientAccountOPD_paid($date,$date1,$title);
+	echo "<tr>";
+		echo "<td>&nbsp;</td>";
+		echo "<td>&nbsp;</td>";
+		echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_discount)."</td>";
+		echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_unpaid)."</td>";
+		echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_hmo)."</td>";
+		echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_phic)."</td>";
+		echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_cashpaid)."</td>";
+		echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_creditCard)."</td>";
+		echo "<td align='right'>&nbsp;".($ro->patientAccountOPD_total)."</td>";
+	echo "</tr>";
 echo "</table>";
 
 }
