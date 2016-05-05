e4b28c999c399b4cb7017be15e701ebfe27cca80
rickyx12 <aizekrysler@gmail.com>
2016-05-05 16:43:51 +0800

    modify and add hide class so when user click the menu it will automatically close

--- Registration/menu/jquery.fixedMenu.js
+++ Registration/menu/jquery.fixedMenu.js
@@ -10,10 +10,19 @@
             menu.find('ul li > a').bind('click',function(){
             if ($(this).parent().hasClass('active')){
                 $(this).parent().removeClass('active');
+                console.log("1");
             }
             else{
                 $(this).parent().parent().find('.active').removeClass('active');
                 $(this).parent().addClass('active');
+                console.log("2");
+                
+                $(".hide").click(function(){
+                	
+                	$("ul li > a").parent().removeClass('active');	
+                	
+                });
+                
             }
             })
         });
