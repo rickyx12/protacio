8ac905a2ccc21f2470a8a59859b175c7c5292a98
rickyx12 <aizekrysler@gmail.com>
2016-05-11 15:39:05 +0800

    change the condition from registrationNo to itemNo

--- COCONUT/patientProfile/removeToCensus.php
+++ COCONUT/patientProfile/removeToCensus.php
@@ -3,9 +3,9 @@ include "../../myDatabase.php";
 
 $ro = new database();
 
-$ro->deleteNow("ipdCensus","registrationNo",$_GET['registrationNo']);
+$ro->deleteNow("ipdCensus","id",$_POST['id']);
 
-header("Location: addToCensus.php?registrationNo=$_GET[registrationNo]");
+//header("Location: addToCensus.php?registrationNo=$_GET[registrationNo]");
 
 
 ?>
