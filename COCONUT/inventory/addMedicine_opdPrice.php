bb9d4455d05368f63db294efef7c807b360c3713
rickyx12 <aizekrysler@gmail.com>
2016-06-08 17:59:40 +0800

    backend for lookup in opd price

---
+++ COCONUT/inventory/addMedicine_opdPrice.php
@@ -0,0 +1,23 @@
+<?
+include "../../myDatabase.php";
+include "../../myDatabase4.php";
+
+$stockCardNo = $_POST['stockCardNo'];
+
+$ro = new database();
+$ro4 = new database4();
+
+$ro4->unitcost_list($stockCardNo);
+echo "<font size=4><b>OPD Price</b></font><Br>";
+echo "<Table>";
+foreach( $ro4->unitcost_list_inventoryCode() as $inventoryCode ) {
+	echo "<Tr>";
+	echo "<td><font size=3>".number_format($ro->selectNow("inventory","opdPrice","inventoryCode",$inventoryCode),2)."</font></td>";
+	echo "<td>&nbsp;<font size=3>-</font>&nbsp;</td>";
+	echo "<td><font size=3>".$ro4->formatDate($ro->selectNow("inventory","dateAdded","inventoryCode",$inventoryCode))."</font></td>";
+	echo "<tr>";
+}
+echo "</table>";
+
+
+?>
