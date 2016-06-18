d4ce81f4dae8b5363723279a61999862f3b80f5e
rickyx12 <aizekrysler@gmail.com>
2016-06-18 17:56:27 +0800

    uncomment opd transaction

--- COCONUT/billing/transactionSummaryCheckBalance.php
+++ COCONUT/billing/transactionSummaryCheckBalance.php
@@ -113,7 +113,8 @@ $stUnpaid;
 $stShare;
 $stDiscount;
 
-//$ro3->showAllAccountTitle_opd($date,$date1);
+$ro3->showAllAccountTitle_opd($date,$date1);
+
 echo "<br>";
 echo "<table border=0 width='80%'>";
 echo "<tr>";
@@ -130,7 +131,7 @@ echo "<th>&nbsp;TOTAL</th>";
 echo "</tr>";
 
 $ro3->showPFaccounts($date,$date1);
-/*
+
 $ro3->showTherapyAccounts($date,$date1,"OT");
 $otTotal = $ro3->showTherapyAccounts_total();
 $otCreditCard = $ro3->showTherapyAccounts_creditCard();
@@ -155,7 +156,7 @@ $stShare = $ro3->showTherapyAccounts_pf();
 $st_Payables = $ro3->showTherapyAccounts_payable();
 $st_hmo_Payables = $ro3->showTherapyAccounts_hmo_payable();
 $stDiscount = $ro3->showTherapyAccounts_discount();
-*/
+
 echo "</table>";
 
 ?>
