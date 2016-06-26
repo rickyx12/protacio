02a698cb82d7ff1f4190037b9321e1ae1dbe8303
rickyx12 <aizekrysler@gmail.com>
2016-06-27 03:44:24 +0800

    add inventoryType variable upon redirection

--- COCONUT/inventory/non-invoice-delete.php
+++ COCONUT/inventory/non-invoice-delete.php
@@ -3,12 +3,13 @@ include "../../myDatabase.php";
 $ro = new database();
 
 $inventoryCode = $_POST['inventoryCode'];
+$inventoryType = $_POST['inventoryType'];
 
 foreach($inventoryCode as $code) {
 	$ro->editNow("inventory","inventoryCode",$code,"status","DELETED_omg_".date("Y-m-d"));
 }
 
-header("Location: non-invoice.php");
+header("Location: non-invoice.php?inventoryType=$inventoryType");
 
 
 ?>
