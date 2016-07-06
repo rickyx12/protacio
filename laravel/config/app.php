129cbad21d39ec33d972f915c639d4be788fdd63
rickyx12 <aizekrysler@gmail.com>
2016-07-06 09:10:01 +0800

    add service provider databaseNow

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -155,7 +155,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        App\Providers\RocketShipServiceProvider::class,
+        App\Providers\databaseNowServiceProvider::class,
     ],
 
     /*
