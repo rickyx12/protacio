f3f22cfe366e382a1b8f52ec5e25fdc630a25673
rickyx12 <aizekrysler@gmail.com>
2016-08-02 15:09:00 +0800

    signout point to session/out.php

--- COCONUT/Cashier/initializeCashier.php
+++ COCONUT/Cashier/initializeCashier.php
@@ -49,7 +49,7 @@ $ro->coconutBoxStart("600","100");
 echo "<br>";
 echo "<span class='style3'>Logged in as $username</span>";
 echo "<Br>";
-echo "<a href='http://".$ro->getMyUrl()."/COCONUT/Cashier/mySession.php'><span class='style2'>&lt;&lt; Sign Out</span></a>&nbsp;&nbsp;&nbsp;";
+echo "<a href='../session/out.php'><span class='style2'>&lt;&lt; Sign Out</span></a>&nbsp;&nbsp;&nbsp;";
 echo "&nbsp;&nbsp;<a href='http://".$ro->getMyUrl()."/COCONUT/Cashier/cashierMainpage.php'><span class='style1'>Sign In &gt;&gt;</span></a>";
 $ro->coconutBoxStop();
 
