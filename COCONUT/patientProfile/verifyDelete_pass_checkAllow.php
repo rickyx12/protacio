11e3ef425b6e85f19fe23adb6d625bae520802a3
rickyx12 <aizekrysler@gmail.com>
2016-08-02 18:26:37 +0800

    disable auto return

--- COCONUT/patientProfile/verifyDelete_pass_checkAllow.php
+++ COCONUT/patientProfile/verifyDelete_pass_checkAllow.php
@@ -20,10 +20,17 @@ $ro = new database2();
 
 
 
+/* 
+pra sa auto return ibig sbhin kpg nag return ung NS,ER,OR ndi n kailangan ng confirmation pra bumalik s inventory ung qty ng nireturn
 if( $ro->selectNow("patientCharges","inventoryFrom","itemNo",$itemNo) != "PHARMACY" && $ro->selectNow("patientCharges","inventoryFrom","itemNo",$itemNo) != "CSR" ) {
 $ro->gotoPage("http://".$ro->getMyUrl()."/COCONUT/patientProfile/itemDepartment.php?itemNo=$itemNo&username=$username&return=main");
 }else {
 $ro->gotoPage("http://".$ro->getMyUrl()."/COCONUT/patientProfile/verifyDelete_redirect_checkAllow.php?registrationNo=$registrationNo&itemNo=$itemNo&description=$description&quantity=$quantity&username=$username&show=$show&desc=$desc");
 }
+*/
+
+//s ngaun disable q muna ung auto return
+$ro->gotoPage("http://".$ro->getMyUrl()."/COCONUT/patientProfile/verifyDelete_redirect_checkAllow.php?registrationNo=$registrationNo&itemNo=$itemNo&description=$description&quantity=$quantity&username=$username&show=$show&desc=$desc");
+
 
 ?>
