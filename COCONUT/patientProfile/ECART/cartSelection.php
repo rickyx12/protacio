5948596ed9cd65f28c6d1f07722117de927c2fba
rickyx12 <aizekrysler@gmail.com>
2016-08-02 18:33:58 +0800

    add NURSING/ER/OR in the selection of inventory

--- COCONUT/patientProfile/ECART/cartSelection.php
+++ COCONUT/patientProfile/ECART/cartSelection.php
@@ -1,4 +1,5 @@
 <?php
+require_once "../../authentication.php";
 include("../../../myDatabase.php");
 $registrationNo = $_GET['registrationNo'];
 $batchNo = $_GET['batchNo'];
@@ -8,6 +9,51 @@ $room = $_GET['room'];
 $ro = new database();
 
 $ro->getPatientProfile($registrationNo);
+
+echo "<script src='../../js/jquery-2.1.4.min.js'></script>";
+echo "<script src='../../js/open.js'></script>";
+
+echo "
+
+	<script>
+		$('document').ready(function() {
+			$('#nursing').click(function(){
+
+				var data = {
+					inventoryLocation:'NURSING',
+					batchNo:'$batchNo',
+					registrationNo:'$registrationNo'
+				};
+
+				open('POST','../../inventory/dept-charges.php',data,'_parent');
+			});
+
+			$('#er').click(function(){
+
+				var data = {
+					inventoryLocation:'E.R',
+					batchNo:'$batchNo',
+					registrationNo:'$registrationNo'
+				};
+
+				open('POST','../../inventory/dept-charges.php',data,'_parent');
+			});
+
+			$('#or').click(function(){
+
+				var data = {
+					inventoryLocation:'OR',
+					batchNo:'$batchNo',
+					registrationNo:'$registrationNo'
+				};
+
+				open('POST','../../inventory/dept-charges.php',data,'_parent');
+			});
+
+		});
+	</script>
+
+";
 
 echo "
 <style type='text/css'>
@@ -40,6 +86,12 @@ echo "<td id='rowz'>&nbsp;<img src='http://".$ro->getMyUrl()."/COCONUT/myImages/
 echo "<td id='rowz'>&nbsp;<img src='http://".$ro->getMyUrl()."/COCONUT/myImages/19.gif'><a href='http://".$ro->getMyUrl()."/COCONUT/availableSupplies/searchSupplies.php?registrationNo=$registrationNo&username=$username&inventoryFrom=PHARMACY&batchNo=$batchNo' target='selectedFrame'>Supplies</a>&nbsp;</td>";
 
 echo "<td id='rowz'>&nbsp;<img src='http://".$ro->getMyUrl()."/COCONUT/myImages/19.gif'><a href='http://".$ro->getMyUrl()."/COCONUT/availableMisc/showMisc.php?registrationNo=$registrationNo&username=$username&inventoryFrom=PHARMACY&batchNo=$batchNo&room=$room' target='selectedFrame'>Misc</a>&nbsp;</td>";
+
+echo "<td id='rowz'>&nbsp;<img src='http://".$ro->getMyUrl()."/COCONUT/myImages/19.gif'><a href='#' id='nursing'>NS STATION</a>&nbsp;</td>";
+
+echo "<td id='rowz'>&nbsp;<img src='http://".$ro->getMyUrl()."/COCONUT/myImages/19.gif'><a href='#' id='er'>E.R</a>&nbsp;</td>";
+
+echo "<td id='rowz'>&nbsp;<img src='http://".$ro->getMyUrl()."/COCONUT/myImages/19.gif'><a href='#' id='or'>OR</a>&nbsp;</td>";
 
 //echo "<td id='rowz'>&nbsp;<img src='http://".$ro->getMyUrl()."/COCONUT/myImages/19.gif'><a href='http://".$ro->getMyUrl()."/COCONUT/madeUpCharges/?registrationNo=$registrationNo&username=$username&room=$room&batchNo=$batchNo' target='selectedFrame'>Other Charges</a>&nbsp;</td>";
 
