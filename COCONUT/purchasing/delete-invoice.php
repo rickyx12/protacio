1542dc336f2c666455d8434730ae48e5ad1bbcd2
rickyx12 <aizekrysler@gmail.com>
2016-08-04 16:50:09 +0800

    delete inventory from inventory tbl if the invoice is deleted

--- COCONUT/purchasing/delete-invoice.php
+++ COCONUT/purchasing/delete-invoice.php
@@ -6,7 +6,12 @@
 
 	$ro = new database();
 
+	$invoiceNo = $ro->selectNow("salesInvoice","invoiceNo","siNo",$siNo);
+
 	$ro->editNow("salesInvoice","siNo",$siNo,"status","DELETED_".$ro->selectNow("registeredUser","username","employeeID",$_SESSION['employeeID']));
 	$ro->editNow("salesInvoiceItems","siNo",$siNo,"status","DELETED_".$ro->selectNow("registeredUser","username","employeeID",$_SESSION['employeeID']));
+
+	$ro->editNow("inventory","invoiceNo",$invoiceNo,"status","DELETED_".$ro->selectNow("registeredUser","username","employeeID",$_SESSION['employeeID']));
+
 
 ?>
