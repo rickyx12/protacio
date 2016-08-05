fb12cd7207bd8ff3fe6a403bf03cae95fc4a15e1
rickyx12 <aizekrysler@gmail.com>
2016-08-05 16:53:04 +0800

    delete also the rows in inventory table

--- COCONUT/purchasing/delete-invoice-item.php
+++ COCONUT/purchasing/delete-invoice-item.php
@@ -6,6 +6,10 @@
 	
 	$ro = new database();
 
+	$inventoryCode = $ro->selectNow("salesInvoiceItems","inventoryCode","refNo",$refNo);
+	$username = $ro->selectNow("registeredUser","username","employeeID",$_SESSION['employeeID']);
+
 	$ro->editNow("salesInvoiceItems","refNo",$refNo,"status","DELETED_".$ro->selectNow("registeredUser","username","employeeID",$_SESSION['employeeID']));
+	$ro->editNow("inventory","inventoryCode",$inventoryCode,"status","DELETED_".$username);
 
 ?>
