daec37a22e6762224b08f37e3c1d090f7ce1e5da
rickyx12 <aizekrysler@gmail.com>
2016-08-12 14:24:53 +0800

    remove unnecessary fields

--- COCONUT/Doctor/addNewDoctorService.php
+++ COCONUT/Doctor/addNewDoctorService.php
@@ -30,7 +30,7 @@ $("#breadcrumbs a").hover(
 echo "<form method='get' action='addNewDoctorService1.php'>";
 echo "<input type=hidden name='discount' value=''>";
 echo "<input type=hidden name='username' value='$username'>";
-echo "<br><center><div style='border:1px solid #000000; width:600px; height:280px; border-color:black black black black;'>";
+echo "<br><center><div style='border:1px solid #000000; width:600px; height:170px; border-color:black black black black;'>";
 echo "<br><Br><table border=0 cellpadding=0 cellspacing=0>";
 echo "<tr>";
 echo "<td><font class='labelz'>Service&nbsp;</font></td>";
@@ -43,6 +43,7 @@ echo "<select name='specialization' class='comboBox'>";
 $ro->showAllSpecialization();
 echo "</select>";
 echo "</td>";
+/*
 echo "</tr>";
 echo "<tr>";
 echo "<td><font class='labelz'>Cash Rate&nbsp;</font></td>";
@@ -78,6 +79,7 @@ echo "<option value='yes'>yes</option>";
 echo "<option value='no'>no</option>";
 $ro->coconutComboBoxStop();
 echo "</td>";
+*/
 echo "</tr>";
 echo "<tr><Td>&nbsp;</tD></tR>";
 echo "<tr>";
--- COCONUT/Doctor/addNewDoctorService1.php
+++ COCONUT/Doctor/addNewDoctorService1.php
@@ -3,17 +3,17 @@ include("../../myDatabase.php");
 $username = $_GET['username'];
 $serviceName = $_GET['serviceName'];
 $specialization = $_GET['specialization'];
-$cashAmount = $_GET['cashAmount'];
-$companyRate = $_GET['companyRate'];
-$doctorShare = $_GET['doctorShare'];
-$discount = $_GET['discount'];
-$phic = $_GET['phic'];
+//$cashAmount = $_GET['cashAmount'];
+//$companyRate = $_GET['companyRate'];
+//$doctorShare = $_GET['doctorShare'];
+//$discount = $_GET['discount'];
+//$phic = $_GET['phic'];
 
 
 $ro = new database();
 
 
 
-$ro->addNewDoctorService($serviceName,$specialization,$cashAmount,$companyRate,$doctorShare,$discount,$username,$phic);
+$ro->addNewDoctorService($serviceName,$specialization,"","","","",$username,"");
 
 ?>
