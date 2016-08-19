a710a3caec75cdba000c5e793b59215accc32839
rickyx12 <aizekrysler@gmail.com>
2016-08-19 20:28:12 +0800

    revert back the original content-type

--- export-to-excel/exporttoexcel.php
+++ export-to-excel/exporttoexcel.php
@@ -7,12 +7,9 @@ if( !isset($_POST['reportName']) ) {
 	header("Pragma: public");
 	header("Expires: 0");
 	header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-	/*
 	header("Content-Type: application/force-download");
 	header("Content-Type: application/octet-stream");
 	header("Content-Type: application/download");
-	*/
-	header("Content-Type:application/vnd.ms-excel; charset=utf-8");
 	header("Content-Disposition: attachment;filename='".$_POST['reportName'].".xls'");
 	header("Content-Transfer-Encoding: binary ");
 
