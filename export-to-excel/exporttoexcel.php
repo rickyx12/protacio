3351a462a2a0936a0eb0721a5904e996c10b7d2f
rickyx12 <aizekrysler@gmail.com>
2016-08-19 18:40:07 +0800

    fix for filename

--- export-to-excel/exporttoexcel.php
+++ export-to-excel/exporttoexcel.php
@@ -1,12 +1,18 @@
 <?php  
-header("Pragma: public");
-header("Expires: 0");
-header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-header("Content-Type: application/force-download");
-header("Content-Type: application/octet-stream");
-header("Content-Type: application/download");
-header("Content-Disposition: attachment;filename=export.xls");
-header("Content-Transfer-Encoding: binary ");
 
-echo strip_tags($_POST['tableData'],'<table><th><tr><td>');  
+if( !isset($_POST['reportName']) ) {
+	echo "Pls provide report name";
+}else {
+	header("Pragma: public");
+	header("Expires: 0");
+	header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+	header("Content-Type: application/force-download");
+	header("Content-Type: application/octet-stream");
+	header("Content-Type: application/download");
+	header("Content-Disposition: attachment;filename='".$_POST['reportName'].".xls'");
+	header("Content-Transfer-Encoding: binary ");
+
+	echo strip_tags($_POST['tableData'],'<table><th><tr><td>');  
+}
+
 ?>
