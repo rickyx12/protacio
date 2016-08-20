0f12e40ec8dea7531c0ee023f3794587da19689d
rickyx12 <aizekrysler@gmail.com>
2016-08-20 14:53:36 +0800

    add export-to-excel function

--- COCONUT/accounting/purchaseJournal.php
+++ COCONUT/accounting/purchaseJournal.php
@@ -1,4 +1,5 @@
 <?php
+
 include("../../myDatabase3.php");
 
 $month = $_GET['month'];
@@ -13,6 +14,8 @@ $ro = new database3();
 $date = $year.$month.$day;
 $date1 = $year1.$month1.$day1;
 
+
+
 $datez_m = substr($date,0,4);
 $datez_d = substr($date,4,2);
 $datez_y = substr($date,6,4);
@@ -22,9 +25,36 @@ $datez_m1 = substr($date1,0,4);
 $datez_d1 = substr($date1,4,2);
 $datez_y1 = substr($date1,6,4);
 
-echo $datez_m."-".$datez_d."-".$datez_y."<Br>";
-echo $datez_m1."-".$datez_d1."-".$datez_y1."<Br>";
+$date_format = $datez_m."-".$datez_d."-".$datez_y;
+$date1_format = $datez_m1."-".$datez_d1."-".$datez_y1;
 
+echo $date_format."<Br>";
+echo $date1_format."<Br>";
+?>
+<script src="../js/jquery-2.1.4.min.js"></script>
+<script>
+	$(document).ready(function(){
+
+		var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+
+		if( isChrome == true ) {
+			$("#export").click(function(){
+				var data='<table>'+$("#purchaseJournal").html().replace(/<a\/?[^>]+>/gi, '')+'</table>';
+				var reportName = '<? echo "Purchase Journal [".$ro->formatDate($date_format)." to ".$ro->formatDate($date1_format)."]" ?>';	
+
+				$('body').prepend("<form method='post' action='../../export-to-excel/exporttoexcel.php' style='display:none' id='ReportTableData'><input type='text' name='tableData' value='"+data+"' ><input type='text' name='reportName' value='"+reportName+"'></form>");
+				$('#ReportTableData').submit().remove();
+				return false;	
+			
+			});
+		}else {
+			$("#export").hide();
+		}	
+
+	});
+</script>
+<?php
+echo "<a href='#' id='export'><img src='../../export-to-excel/excel-icon.png'></a>";
 $ro->accounting_purchaseJournal($date,$date1);
 
 
