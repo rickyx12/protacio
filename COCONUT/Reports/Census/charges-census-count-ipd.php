2754b92b163b7f2457f0297472f6ee34f6544bda
rickyx12 <aizekrysler@gmail.com>
2016-08-25 14:13:21 +0800

    count how many procedure done in IPD

--- COCONUT/Reports/Census/charges-census-count-ipd.php
+++ COCONUT/Reports/Census/charges-census-count-ipd.php
@@ -1,14 +1,22 @@
 <?
 	
+	include "../../../myDatabase.php";
 	include "../../../myDatabase4.php";
 
 	$chargesCode = $_POST['chargesCode'];
 	$date1 = $_POST['date1'];
 	$date2 = $_POST['date2'];
 
+	$ro = new database();
 	$ro4 = new database4();
 
-	echo $ro4->count_charges($chargesCode,$date1,$date2,"IPD");
-	
+	$price = $ro->selectNow("availableCharges","WARD","chargesCode",$chargesCode);
+	$census = $ro4->count_charges($chargesCode,$date1,$date2,"IPD");
+
+	if( $census > 0 ) {
+		echo "(".$census.") ".number_format(($price * $census),2);
+	}else {
+
+	}
 
 ?>
