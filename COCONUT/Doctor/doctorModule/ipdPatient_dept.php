a59b4b5cf0c092997eed5bc441b9d6dc1eda31ff
rickyx12 <aizekrysler@gmail.com>
2016-08-25 15:23:47 +0800

    change format and patient name will be button

--- COCONUT/Doctor/doctorModule/ipdPatient_dept.php
+++ COCONUT/Doctor/doctorModule/ipdPatient_dept.php
@@ -5,7 +5,7 @@ $ro = new database();
 
 $ro->coconutDesign();
 
-$ro->coconutBoxStart("600","auto");
+$ro->coconutBoxStart("1000","auto");
 echo "<Br>";
 $ro->getDoctorPatient_ipdCensus($doctor,"IPD");
 echo "<br>";
