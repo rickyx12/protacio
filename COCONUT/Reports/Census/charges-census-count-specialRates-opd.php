98a541db2460c4ffc1e44a868f41a5603c971e0f
rickyx12 <aizekrysler@gmail.com>
2016-08-25 14:25:34 +0800

    census for OPD special rates

---
+++ COCONUT/Reports/Census/charges-census-count-specialRates-opd.php
@@ -0,0 +1,21 @@
+<?
+	include "../../../myDatabase.php";
+	include "../../../myDatabase4.php";
+
+	$chargesCode = $_POST['chargesCode'];
+	$date1 = $_POST['date1'];
+	$date2 = $_POST['date2'];
+
+	$ro = new database();
+	$ro4 = new database4();
+
+	$price = $ro->selectNow("availableCharges","specialRates","chargesCode",$chargesCode);
+	$census = $ro4->count_charges($chargesCode,$date1,$date2,"specialRates_opd");
+
+	if( $census > 0 ) {
+		echo "(".$census.") ".number_format(($price * $census),2);
+	}else {
+
+	}
+
+?>
