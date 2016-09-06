b2989471c0151f4005ae9f2ec3f1e1faf786f133
rickyx12 <aizekrysler@gmail.com>
2016-09-06 19:28:10 +0800

    fix for date

--- COCONUT/Pharmacy/monitoring/monitoring.php
+++ COCONUT/Pharmacy/monitoring/monitoring.php
@@ -1,13 +1,6 @@
 <?php
 include("../../../myDatabase2.php");
 $inventoryCode = $_GET['inventoryCode'];
-$month = $_GET['month'];
-$day = $_GET['day'];
-$year = $_GET['year'];
-
-$month1 = $_GET['month1'];
-$day1 = $_GET['day1'];
-$year1 = $_GET['year1'];
 
 
 $ro = new database2();
@@ -16,11 +9,15 @@ echo "<center><font size=5>".$ro->selectNow("inventory","description","inventory
 echo "<center>Beginning QTY ".$ro->selectNow("inventory","encodedQTY","inventoryCode",$inventoryCode)."</center>";
 echo "<center>Remaining QTY ".$ro->selectNow("inventory","quantity","inventoryCode",$inventoryCode)."</center>";
 
-$ro->dispensedMonitor($inventoryCode,$month,$day,$year,$month1,$day1,$year1);
+$date1 = date("Y")."-01-01";
+$date2 = date("Y")."-12-31";
+
+$ro->dispensedMonitor($inventoryCode,$date1,$date2);
 
 echo "<br><br>";
 
-$ro->getRequestedDept($inventoryCode,$year."-".$month."-".$day,$year1."-".$month1."-".$day1);
+
+//$ro->getRequestedDept($inventoryCode,$date1,$date2);
 
 
 
