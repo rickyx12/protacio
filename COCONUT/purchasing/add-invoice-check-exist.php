6ed71c0d5949bb37cb38d460e98718e106d1effd
rickyx12 <aizekrysler@gmail.com>
2016-09-08 17:36:47 +0800

    allow same invoiceNo# but different supplier

--- COCONUT/purchasing/add-invoice-check-exist.php
+++ COCONUT/purchasing/add-invoice-check-exist.php
@@ -2,10 +2,11 @@
 	require_once "../authentication.php";
 	include "../../myDatabase.php";
 	$invoiceNo = $_POST['invoiceNo'];
+	$supplier = $_POST['supplier'];
 
 	$ro = new database();
 
-	if( $ro->doubleSelectNow('salesInvoice','siNo','invoiceNo',$invoiceNo,"status","Active") != "" ) {
+	if( $ro->tripleSelectNow('salesInvoice','siNo','invoiceNo',$invoiceNo,"status","Active","supplier",$supplier) != "" ) {
 		echo "1";
 	}else {
 		echo "2";
