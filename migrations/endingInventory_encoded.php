7183028bae7a167df684a8b20f604d68ab5bd1d2
rickyx12 <aizekrysler@gmail.com>
2016-09-28 23:51:23 +0800

    add column encoded in endingInventory tbl

---
+++ migrations/endingInventory_encoded.php
@@ -0,0 +1,26 @@
+<?php
+$servername = $_SERVER['DB_HOST'];
+$username = $_SERVER['DB_USER'];
+$password = $_SERVER['DB_PASS'];
+$dbname = $_SERVER['DB_DB'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "ALTER TABLE endingInventory
+ADD COLUMN encoded VARCHAR(200) NOT NULL;
+";
+
+if ($conn->query($sql) === TRUE) {
+    echo "ok";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
