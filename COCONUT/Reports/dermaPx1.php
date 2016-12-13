23b844408dd7313788d3e77be321ae7cce4c344f
rickyx12 <aizekrysler@gmail.com>
2016-12-14 00:10:31 +0800

    update only medicine/supplies/doctor icasiano as derma

--- COCONUT/Reports/dermaPx1.php
+++ COCONUT/Reports/dermaPx1.php
@@ -5,9 +5,6 @@ $count = count($registrationNo);
 
 $ro = new database3();
 
-//$ro->dermaCharges("101980");
-
-
 for($x=0;$x<$count;$x++) {
 echo "Reg#:&nbsp;".$registrationNo[$x]."<Br>";
 $ro->dermaCharges($registrationNo[$x]);
@@ -18,6 +15,7 @@ if( $ro->dermaCharges_itemNo() != "" ) {
 
 	foreach( $ro->dermaCharges_itemNo() as $itemNo ) {
 
+		/* DATING CONDITIONS PRA S DERMA
 		if( $ro->selectNow("patientCharges","description","itemNo",$itemNo) == "MEDICAL CERTIFICATE" ) {
 			//dont update
 		}else if( $ro->selectNow("patientCharges","description","itemNo",$itemNo) == "SSS Record / Medical Records" ) {
@@ -36,7 +34,25 @@ if( $ro->dermaCharges_itemNo() != "" ) {
 		else {
 			$ro->editNow("patientCharges","itemNo",$itemNo,"title","DERMA");
 		}
+		*/
 
+		//UPDATE ONLY IF THE CHARGES IS MED/SUP/DOC ICASIANO [DEC 11,2016]
+		if( $ro->selectNow('patientCharges','title','itemNo',$itemNo) == 'MEDICINE' ) {
+			$ro->editNow("patientCharges","itemNo",$itemNo,"title","DERMA");
+		}else if( $ro->selectNow('patientCharges','title','itemNo',$itemNo) == 'SUPPLIES' ) {
+			$ro->editNow("patientCharges","itemNo",$itemNo,"title","DERMA");
+		}else if( $ro->selectNow('patientCharges','title','itemNo',$itemNo) == 'PROFESSIONAL FEE' ) {
+			if( $ro->selectNow("patientCharges","chargesCode","itemNo",$itemNo) == "17" ) { // kpg c doc icasiano update title from PF to DERMA.. "17" is the doctorCode in Doctors Table
+				$ro->editNow("patientCharges","itemNo",$itemNo,"title","DERMA");
+				//update ung sellingPrice cols kpg c doc icasiano pra ndi mag error.. since derma n sya dpat wlang "/" ung sellingPrice
+				$ro->editNow("patientCharges","itemNo",$itemNo,"sellingPrice",$ro->selectNow("patientCharges","total","itemNo",$itemNo)); 
+			}else {
+				//dont update the doctor unless c doc icasiono ung doctor
+			}				
+		}
+		else {
+			//DONT UPDATE
+		}
 	}
 
 }
